1488e4574e6761956bc37fe92a89b59bbded2582
Dan <dan@borellos.com>
2015-11-18 10:16:54 -0800

    Cleaned up code

--- PID.py
+++ PID.py
@@ -1,59 +1,63 @@
-import time, logging, logging.config
+import time
+import logging
+import logging.config
 
-#Start logging
+# Start logging
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 
+
 class PID:
-	def __init__(self,Kp,Ki,Kd,I_min,I_max):
+	def __init__(self, Kp, Ki, Kd, I_min, I_max):
 		self.Kp = Kp
 		self.Ki = Ki
 		self.Kd = Kd
-		
+
 		self.P = 0.0
 		self.I = 0.0
 		self.D = 0.0
-		
+		self.PID = 0
+
 		self.Derv = 0.0
 		self.Inter = 0.0
 		self.Inter_max = I_max
 		self.Inter_min = I_min
-		
+
 		self.setTarget(0.0)
-		
-	def update(self,Current):
+
+	def update(self, Current):
 		error = Current - self.setPoint
-		
+
 		dT = time.time() - self.LastUpdate
 		self.Inter += error*dT
-		self.Inter = min(max(self.Inter,self.Inter_min),self.Inter_max)
-		
+		self.Inter = min(max(self.Inter, self.Inter_min), self.Inter_max)
+
 		self.Derv = (error - self.error)/dT
-		
+
 		self.P = self.Kp * error
 		self.I = self.Ki * self.Inter
 		self.D = self.Kd * self.Derv
 		self.PID = self.P + self.I + self.D
-		
+
 		self.error = error
 		self.LastUpdate = time.time()
-		
+
 		logger.info('Target: %d Current: %d Gains: (%f,%f,%f) P: %f I: %f D: %f Adjustments: (%f,%f,%f) PID: %f' ,self.setPoint, Current,self.Kp,self.Ki,self.Kd,error,self.Inter,self.Derv,self.P,self.I,self.D,self.PID)
-		
+
 		return self.PID
-		
-	def	setTarget(self,setPoint):
+
+	def	setTarget(self, setPoint):
 		self.setPoint = setPoint
 		self.error = 0.0
 		self.Inter = 0.0
 		self.Derv = 0.0
 		self.LastUpdate = time.time()
-		
-	def setGains(self,Kp,Ki,Kd):
+
+	def setGains(self, Kp, Ki, Kd):
 		self.Kp = Kp
 		self.Ki = Ki
 		self.Kd = Kd
-		logger.info('New Gains (%f,%f,%f)',Kp,Ki,Kd)
-	
+		logger.info('New Gains (%f,%f,%f)', Kp, Ki, Kd)
+
 	def getK(self):
-		return (self.Kp, self.Ki, self.Kd)
+		return self.Kp, self.Ki, self.Kd
--- README.md
+++ README.md
@@ -5,4 +5,3 @@
 * spidev
 * [Adafruit_Python_CharLCD](https://github.com/adafruit/Adafruit_Python_CharLCD)
 * [Python-firebase](https://github.com/ozgur/python-firebase) Use github version instead of pip
-Test 3
