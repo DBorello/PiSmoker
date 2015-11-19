7fc140d14fe958a851e6447411e33b61d91b6c40
Dan <dan@borellos.com>
2015-11-18 21:39:34 -0800

    Moved PIDv2 to PID

--- PID.py
+++ PID.py
@@ -6,12 +6,15 @@ import logging.config
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 
+#PID controller based on proportional band in standard PID form https://en.wikipedia.org/wiki/PID_controller#Ideal_versus_standard_PID_form
+# u = Kp (e(t)+ 1/Ti INT + Td de/dt)
+# PB = Proportional Band
+# Ti = Goal of eliminating in Ti seconds
+# Td = Predicts error value at Td in seconds
 
 class PID:
-	def __init__(self, Kp, Ki, Kd, I_min, I_max):
-		self.Kp = Kp
-		self.Ki = Ki
-		self.Kd = Kd
+	def __init__(self,  PB, Ti, Td, I_max):
+		self.CalculateGains(PB,Ti,Td)
 
 		self.P = 0.0
 		self.I = 0.0
@@ -21,34 +24,40 @@ class PID:
 		self.Derv = 0.0
 		self.Inter = 0.0
 		self.Inter_max = I_max
-		self.Inter_min = I_min
 
 		self.setTarget(0.0)
+
+	def CalculateGains(self,PB,Ti,Td):
+		self.Kp = -1/PB
+		self.Ki = self.Kp/Ti
+		self.Kd = self.Kp*Td
 
 	def update(self, Current):
 		#P
 		error = Current - self.setPoint
+		self.P = self.Kd + 0.5 #P = 1 for PB/2 under setPoint, P = 0 for PB/2 over setPoint
 
 		#I
 		dT = time.time() - self.LastUpdate
-		self.Inter += error*dT
-		self.Inter = max(self.Inter, self.Inter_min)
-		self.Inter = min(self.Inter, self.Inter_max)
+		if self.P > 0 and self.P < 1: #Ensure we are in the PB, otherwise do not calculate I to avoid windup
+			self.Inter += error*dT
+			self.Inter = max(self.Inter, -self.Inter_max)
+			self.Inter = min(self.Inter, -self.Inter_max)
+
+		self.I = self.Ki * self.Inter
 
 		#D
 		self.Derv = (error - self.error)/dT
+		self.D = self.Kd * self.Derv
 
 		#PID
-		self.P = self.Kp * error
-		self.I = self.Ki * self.Inter
-		self.D = self.Kd * self.Derv
 		self.PID = self.P + self.I + self.D
 
 		#Update for next cycle
 		self.error = error
 		self.LastUpdate = time.time()
 
-		logger.info('Target: %d Current: %d Gains: (%f,%f,%f) P: %f I: %f D: %f Adjustments: (%f,%f,%f) PID: %f' ,self.setPoint, Current,self.Kp,self.Ki,self.Kd,error,self.Inter,self.Derv,self.P,self.I,self.D,self.PID)
+		logger.info('Target: %d Current: %d Gains: (%f,%f,%f) Errors(%f,%f,%f) Adjustments: (%f,%f,%f) PID: %f' ,self.setPoint, Current,self.Kd,self.Ki,self.Kd,error,self.Inter,self.Derv,self.P,self.I,self.D,self.PID)
 
 		return self.PID
 
@@ -58,12 +67,11 @@ class PID:
 		self.Inter = 0.0
 		self.Derv = 0.0
 		self.LastUpdate = time.time()
+		logger.info('New Target: %f',setPoint)
 
-	def setGains(self, Kp, Ki, Kd):
-		self.Kp = Kp
-		self.Ki = Ki
-		self.Kd = Kd
-		logger.info('New Gains (%f,%f,%f)', Kp, Ki, Kd)
+	def setGains(self, PB, Ti, Td):
+		self.CalculateGains(PB,Ti,Kd)
+		logger.info('New Gains (%f,%f,%f)', self.Kp, self.Ki, self.Kd)
 
 	def getK(self):
 		return self.Kp, self.Ki, self.Kd
